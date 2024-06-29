5c10fd9bf77f1653513243b8e41e6395a4466587
ipunganwar <ipung.anwar1@gmail.com>
2024-06-29 16:06:48 +0700

    feat: add seed func

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -14,6 +14,11 @@ const main = async () => {
 
     await db.delete(schema.courses);
     await db.delete(schema.userProgress);
+    await db.delete(schema.units);
+    await db.delete(schema.lessons);
+    await db.delete(schema.challenges);
+    await db.delete(schema.challengeOptions);
+    await db.delete(schema.challengeProgress);
 
     await db.insert(schema.courses).values([
       { id: 1, title: "Spanish", imageSrc: "/es.svg" },
@@ -21,6 +26,31 @@ const main = async () => {
       { id: 3, title: "Italian", imageSrc: "/it.svg" },
       { id: 4, title: "French", imageSrc: "/fr.svg" },
       { id: 5, title: "Croation", imageSrc: "/hr.svg" },
+    ]);
+
+    await db.insert(schema.units).values([
+      {
+        id: 1,
+        courseId: 1,
+        title: "Unit 1",
+        description: "learn the basics of Spanish",
+        order: 1,
+      },
+    ]);
+
+    await db.insert(schema.lessons).values([
+      {
+        id: 1,
+        unitId: 1,
+        order: 1,
+        title: "None",
+      },
+      {
+        id: 2,
+        unitId: 1,
+        order: 2,
+        title: "Verbs",
+      },
     ]);
     console.log("seeding finised");
   } catch (error) {
