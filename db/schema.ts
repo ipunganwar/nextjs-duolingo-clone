4fcc8476b06c7024b30f5ccea00d4ffabe70509c
ipunganwar <ipung.anwar1@gmail.com>
2024-06-29 11:47:20 +0700

    chore: add relations in schema

--- db/schema.ts
+++ db/schema.ts
@@ -74,6 +74,7 @@ export const challengesRelations = relations(challenges, ({ one, many }) => ({
     references: [lessons.id],
   }),
   challengeOptions: many(challengeOptions),
+  challengeProgress: many(challengeProgress),
 }));
 
 export const challengeOptions = pgTable("challengeOptions", {
@@ -92,6 +93,25 @@ export const challengeOptionsRelations = relations(
   ({ one }) => ({
     challenge: one(challenges, {
       fields: [challengeOptions.challengeId],
+      references: [challenges.id],
+    }),
+  })
+);
+
+export const challengeProgress = pgTable("challengeProgress", {
+  id: serial("id").primaryKey(),
+  userId: text("user_id").notNull(),
+  challengeId: integer("challenge_id")
+    .references(() => challenges.id, { onDelete: "cascade" })
+    .notNull(),
+  completed: boolean("completed").notNull().default(false),
+});
+
+export const challengeProgressRelations = relations(
+  challengeProgress,
+  ({ one }) => ({
+    challenge: one(challenges, {
+      fields: [challengeProgress.challengeId],
       references: [challenges.id],
     }),
   })
