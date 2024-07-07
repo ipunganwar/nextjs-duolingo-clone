c3840bf19013a5ec9ada354d74e887392cee0785
ipunganwar <ipung.anwar1@gmail.com>
2024-07-07 12:44:36 +0700

    fix: unit banner

--- app/(main)/learn/unit-banner.tsx
+++ app/(main)/learn/unit-banner.tsx
@@ -14,7 +14,7 @@ export const UnitBanner = ({ title, description }: Props) => {
         <p className="text-lg">{description}</p>
       </div>
 
-      <Link href="/lessons">
+      <Link href="/lesson">
         <Button
           size="lg"
           variant="secondary"
